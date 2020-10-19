2a534260b7cd860f08c10771bde2b7983e3e3b53
wkedarprdxn <kedarw@prdxn.com>
2020-10-19 18:31:52 +0530

    jquery logic for filter added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,14 @@
 /* Author: 
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    $('.filter-btn').click(function() {
+        var val = $(this).attr('data-filter');
+        if (val == "all") {
+            $('.filter').show('1000');
+        } else {
+            $('.filter').not('.' + val).hide('3000');
+            $('.filter').filter('.' + val).show('3000');
+        }
+    })
+});
