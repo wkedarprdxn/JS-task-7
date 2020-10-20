243a36b88a0a46044b34f52c70ef06f7036193b4
wkedarprdxn <kedarw@prdxn.com>
2020-10-20 15:16:32 +0530

    js for responsive nav added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-/* Author: 
-
-*/
 $(document).ready(function() {
     $('.filter-btn').click(function() {
         var val = $(this).attr('data-filter');
@@ -10,5 +7,27 @@ $(document).ready(function() {
             $('.filter').not('.' + val).hide(1000);
             $('.filter').filter('.' + val).show(1000);
         }
-    })
+    });
+    var ulData = $('nav ul');
+    $('.tog-wrap').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('change');
+        ulData.slideToggle();
+    });
+    $(window).resize(function() {
+        var obtener_ancho = $(this).width();
+        if (obtener_ancho > 995 && ulData.is(':hidden')) {
+            ulData.removeAttr('style');
+        }
+    });
+    $('nav ul li').on('click', function() {
+        console.log('nav li clicked');
+        var obtener_ancho = $(window).width();
+        if (obtener_ancho < 995) {
+            $('.tog-wrap').removeClass('change');
+            ulData.slideToggle();
+        }
+    });
+
+
 });
